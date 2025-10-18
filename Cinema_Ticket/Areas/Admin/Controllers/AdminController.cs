ba9f42241d1283793bd24c978ec253b483499d8a
walidmohamedfarag <walid492003@gmail.com>
2025-10-18 22:34:14 +0300

    add dashboard

---
+++ Cinema_Ticket/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cinema_Ticket.Areas.Admin.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Cinema_Ticket/Program.cs
+++ Cinema_Ticket/Program.cs
@@ -27,7 +27,7 @@ namespace Cinema_Ticket
             app.MapStaticAssets();
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}")
+                pattern: "{area=Admin}/{controller=Home}/{action=Index}/{id?}")
                 .WithStaticAssets();
 
             app.Run();
